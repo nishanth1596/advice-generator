e7f2b6b4702e3bd6832f80e65f0dfa1f2c7b3a36
Nishanth <nishanth1596@gmail.com>
2025-04-02 07:30:44 +0530

    Project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import patternMobileIcon from "./assets/images/pattern-divider-mobile.svg";
+import patterDesktopIcon from "./assets/images/pattern-divider-desktop.svg";
+import buttonDiceIcon from "./assets/images/icon-dice.svg";
 
 type InitialAdviceProps = {
   id: number;
@@ -26,25 +29,32 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    fetchAdviseData();
-  }, []);
-
   return (
     <div className="text-LightCyan mx-4 flex h-dvh items-center justify-center">
       <header className="sr-only">
         <h1>Advice Generator</h1>
       </header>
 
-      <main className="bg-DarkGrayishBlue dropShadow w-full px-6 pt-10 text-center">
-        <h2 className="text-NeonGreen text-[0.69rem] leading-[0.94rem] font-extrabold tracking-[3.48px] uppercase">
+      <main className="bg-DarkGrayishBlue dropShadow relative flex w-full max-w-[33.75rem] flex-col items-center rounded-[10px] px-6 pt-10 pb-16 text-center sm:rounded-[15px] sm:px-12 sm:pt-12 sm:pb-[4.5rem]">
+        <h2 className="text-NeonGreen text-[0.69rem] leading-[0.94rem] font-extrabold tracking-[3.48px] uppercase sm:text-[0.81rem] sm:leading-[1.11rem] sm:tracking-[4.09px]">
           Advice #{adviceData.id}
         </h2>
         <p className="text-LightCyan mt-6 text-2xl leading-[2.06rem] font-extrabold tracking-[-0.26px]">
           &quot;{adviceData.advice}&quot;
         </p>
-        <button onClick={fetchAdviseData} className="cursor-pointer">
-          X
+
+        <picture>
+          <source srcSet={patternMobileIcon} media="(max-width: 767px)" />
+          <source srcSet={patterDesktopIcon} media="(min-width: 768px)" />
+          <img src={patternMobileIcon} alt="" className="mt-6 sm:mt-10" />
+        </picture>
+
+        <button
+          onClick={fetchAdviseData}
+          className="bg-NeonGreen btnDropShadow absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 cursor-pointer rounded-full p-5 transition-all duration-300 ease-in-out"
+          aria-label="click to generate next advice"
+        >
+          <img src={buttonDiceIcon} alt="" className="" />
         </button>
       </main>
     </div>
